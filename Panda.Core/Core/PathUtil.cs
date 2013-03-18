b24a8b431765356310e4606c36236527a90f21ae
Christian Klauser <christianklauser@outlook.com>
2013-03-18 21:25:05 +0100

    Add PathUtil class.

---
+++ Panda.Core/Core/PathUtil.cs
@@ -0,0 +1,13 @@
+﻿namespace Panda.Core
+{
+    public class PathUtil
+    {
+        // TODO implement path parsing in this class
+ 
+        // one example would be to take a relative path as a String
+        // and turn it into an array of "path segments".
+
+        // The Navigate implementation can then "react" to each of these segments
+        //  i.e. for a "..", move up, for a  "." do nothing, otherwise lookup the entry
+    }
+}
