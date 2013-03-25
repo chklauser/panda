428ff1c337777452fc6918e05af073c6cfc2f639
Christian Klauser <christianklauser@outlook.com>
2013-03-25 19:20:46 +0100

    Begin implementing RawBlockManager (IRawPersistenceSpace based BlockManager)

---
+++ Panda.Core/Core/IO/RawBlockManager.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+using Panda.Core.Blocks;
+
+namespace Panda.Core.IO
+{
+    public class RawBlockManager : IBlockManager
+    {
+        protected internal const int RootDirectoryFieldOffset = 2;
+        protected internal  const int BlockSizeFieldOffset = 1;
+        protected internal  const int BlockCountFieldOffset = 0;
+
+        [NotNull]
+        private readonly IRawPersistenceSpace _space;
+
+        public RawBlockManager(IRawPersistenceSpace space)
+        {
+            _space = space;
+        }
+
+        private unsafe void* _blockAt(BlockOffset offset)
+        {
+            var bytePtr = (byte*) _space.Pointer;
+            return bytePtr + BlockSize*offset.Offset;
+        }
+
+        public int BlockSize
+        {
+            get { return DataBlockSize; }
+        }
+
+        #region BlockManager API
+
+        public IDirectoryBlock AllocateDirectoryBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDirectoryContinuationBlock AllocateDirectoryContinuationBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileBlock AllocateFileBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileContinuationBlock AllocateFileContinuationBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlockOffset AllocateDataBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FreeBlock(BlockOffset blockOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDirectoryBlock GetDirectoryBlock(BlockOffset blockOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDirectoryContinuationBlock GetDirectoryContinuationBlock(BlockOffset blockOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileBlock GetFileBlock(BlockOffset blockOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileContinuationBlock GetFileContinuationBlock(BlockOffset blockOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public unsafe void WriteDataBlock(BlockOffset blockOffset, byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            var dataBlockSize = DataBlockSize;
+
+            if (data.Length > dataBlockSize)
+                throw new ArgumentException(
+                    string.Format("Data array is longer than block size. (block size = {0}, data length = {1})",
+                        dataBlockSize, data.Length), "data");
+
+            // Copy the provided data
+            var blockAddress = _blockAt(blockOffset);
+            Marshal.Copy(data,0,(IntPtr)blockAddress,data.Length);
+
+            // Padd rest with 0
+            // (this is not really necessary, but it will help us debug)
+            var start = (byte*) blockAddress;
+            var end = start + dataBlockSize;
+            for (var ptr = start + data.Length; ptr < end; ptr++)
+                *ptr = 0;
+        }
+
+        public unsafe void ReadDataBlock(BlockOffset blockOffset, byte[] destination, int destinationIndex = 0,
+                                  int blockIndex = 0,
+                                  int? count = null)
+        {
+            if (destination == null)
+                throw new ArgumentNullException("destination");
+            if(destinationIndex < 0)
+                throw new ArgumentOutOfRangeException("destinationIndex",destinationIndex,"Destination index cannot be negative.");
+            if(blockIndex < 0)
+                throw new ArgumentOutOfRangeException("blockIndex",blockIndex,"Block index cannot be negative.");
+
+            // Check block index
+            var blockRemainingLength = BlockSize - blockIndex;
+            if (blockRemainingLength < 0)
+                throw new ArgumentOutOfRangeException("blockIndex",blockIndex,
+                    string.Format(
+                        "Index into data block is beyond block boundary. (block size = {0})",BlockSize));
+            
+            // Check destination index
+            var destinationRemainingLength = destination.Length - destinationIndex;
+            if (destinationRemainingLength < 0)
+                throw new ArgumentOutOfRangeException("destinationIndex",destinationIndex,
+                    string.Format(
+                        "Index into destination is beyond block boundary. (destination length = {0})",
+                        destination.Length));
+
+            // Check count
+            var actualCount = count ?? Math.Min(destinationRemainingLength, blockRemainingLength);
+            if (actualCount > destinationRemainingLength || actualCount > blockRemainingLength)
+                throw new ArgumentOutOfRangeException("count", count,
+                    "Read count is larger than either the remaining block or the remaining destination array.");
+            if(actualCount < 0)
+                throw new ArgumentOutOfRangeException("count",count,"Count cannot be negative.");
+
+            // Perform copy
+            var ptr = (IntPtr)_blockAt(blockOffset);
+            Marshal.Copy(IntPtr.Add(ptr,blockIndex),destination,destinationIndex, actualCount);
+        }
+
+        public unsafe uint TotalBlockCount
+        {
+            get
+            {
+                var startBlock = (uint*)_space.Pointer;
+                return startBlock[BlockCountFieldOffset];
+            }
+        }
+
+        public unsafe BlockOffset RootDirectoryBlockOffset
+        {
+            get
+            {
+                var startBlock = (uint*)_space.Pointer;
+                return (BlockOffset) startBlock[RootDirectoryFieldOffset];
+            }
+        }
+
+        public unsafe int DataBlockSize
+        {
+            get
+            {
+                var startBlock = (uint*) _space.Pointer;
+                return (int) startBlock[BlockSizeFieldOffset];
+            }
+        }
+
+        #endregion
+
+        protected IRawPersistenceSpace Space
+        {
+            get { return _space; }
+        }
+
+    }
+}
---
+++ Panda.Test/Unit/RawBlockManagerTests.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Panda.Core.Blocks;
+using Panda.Core.IO;
+using Panda.Core.IO.InMemory;
+
+namespace Panda.Test.Unit
+{
+    [TestFixture]
+    public class RawBlockManagerTests : IDisposable
+    {
+        public const uint DefaultBlockSize = 16;
+
+        public IRawPersistenceSpace Space;
+        public IBlockManager BlockManager;
+        public uint BlockCount;
+        public uint BlockSize;
+
+        public unsafe void CreateSpace(uint blockCount = 16, uint blockSize = DefaultBlockSize)
+        {
+            Space = new InMemorySpace(blockCount*blockSize);
+            var ptr = (uint*) Space.Pointer;
+            ptr[RawBlockManager.BlockCountFieldOffset] = BlockCount = blockCount;
+            ptr[RawBlockManager.BlockSizeFieldOffset] = BlockSize = blockSize;
+            BlockManager = new RawBlockManager(Space);
+        }
+
+        public unsafe byte GetByteAt(uint blockOffset, int blockIndex)
+        {
+            var ptr = (byte*) Space.Pointer;
+            return ptr[blockOffset * BlockSize + blockIndex];
+        }
+
+        public unsafe void SetByteAt(uint blockOffset, int blockIndex, byte value)
+        {
+            var ptr = (byte*)Space.Pointer;
+            ptr[blockOffset * BlockSize + blockIndex] = value;
+        }
+
+        public unsafe void SetUInt32At(uint blockOffset, int uintIndex, uint value)
+        {
+            var ptr = (byte*)Space.Pointer;
+            var uintPtr = (uint*) (ptr + BlockSize*blockOffset + sizeof (uint)*uintIndex);
+            *uintPtr = value;
+        }
+
+        public unsafe uint GetUInt32At(uint blockOffset, int uintIndex)
+        {
+            var ptr = (byte*)Space.Pointer;
+            var uintPtr = (uint*)(ptr + BlockSize * blockOffset + sizeof(uint) * uintIndex);
+            return *uintPtr;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+
+        [Test]
+        public void WriteSingleByte()
+        {
+            CreateSpace();
+
+            BlockManager.WriteDataBlock((BlockOffset) 1, new byte[] {0xEF});
+
+            Assert.That(GetByteAt(1,0),Is.EqualTo(0xEF));
+        }
+
+        [Test]
+        public void ReadSingleByte()
+        {
+            CreateSpace();
+
+            const byte sentinel = 0xFE;
+            SetByteAt(1,0, sentinel);
+
+            var dest = new byte[1];
+            BlockManager.ReadDataBlock((BlockOffset) 1,dest);
+
+            Assert.That(dest[0],Is.EqualTo(sentinel));
+        }
+
+        [Test]
+        public void ReadSingleByteAtAnyPosIntoLarge(
+            // Use a number of parameter combinations
+            // Yes, the expressions have to be that complicated, because NUnit is very, very
+            // particular about the exact argument type
+            [Values(0, 3)] int destIndex, 
+            [Values(0, (int) DefaultBlockSize/2-1, (int) DefaultBlockSize-1)] int blockIndex,
+            [Values(4, (int) DefaultBlockSize, (int) (2u * DefaultBlockSize))] int arrayLen)
+        {
+            CreateSpace();
+
+            const byte sentinel = 0xFE;
+            SetByteAt(1, blockIndex, sentinel);
+
+            var dest = new byte[arrayLen];
+            BlockManager.ReadDataBlock((BlockOffset)1, dest,destIndex,blockIndex,1);
+
+            Assert.That(dest[destIndex], Is.EqualTo(sentinel));
+        }
+
+        [Test]
+        public void ReadRootDirectoryBlockOffset()
+        {
+            CreateSpace();
+
+            SetUInt32At(0,RawBlockManager.RootDirectoryFieldOffset,0x15);
+
+            Assert.That(BlockManager.RootDirectoryBlockOffset, Is.EqualTo((BlockOffset) 0x15),
+                "RootBlockOffset at " + RawBlockManager.RootDirectoryFieldOffset);
+        }
+
+        [Test, ExpectedException(typeof (ArgumentNullException))]
+        public void WriteDataNullCheck()
+        {
+            CreateSpace();
+
+// ReSharper disable AssignNullToNotNullAttribute
+            BlockManager.WriteDataBlock((BlockOffset) 1,null);
+// ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test, ExpectedException(typeof (ArgumentException))]
+        public void WriteDataTooLarge([Values(16u,513u)] uint blockSize)
+        {
+            CreateSpace(blockSize:blockSize);
+
+            var data = new byte[blockSize+1];
+            BlockManager.WriteDataBlock((BlockOffset) 1,data);
+        }
+
+        [Test, ExpectedException(typeof (ArgumentNullException))]
+        public void ReadDataNullCheck()
+        {
+            CreateSpace();
+
+            BlockManager.ReadDataBlock((BlockOffset) 1,null);
+        }
+
+        [Test, ExpectedException(typeof (ArgumentOutOfRangeException))]
+        public void ReadDataBlockIndexOutOfRange([Values((int) DefaultBlockSize+1,(int) DefaultBlockSize+15)] int blockIndex)
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize];
+            BlockManager.ReadDataBlock((BlockOffset) 1, data, blockIndex: blockIndex);
+        }
+
+        [Test]
+        public void ZeroInferredForMaxBlockSizeIndex()
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, blockIndex: (int) BlockSize);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ReadDataDestinationIndexOutOfRange([Values(0,1)] int delta)
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize/2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data,(int) (BlockSize/2 + delta),count:1);
+        }
+
+        [Test]
+        public void ZeroInferredForCount()
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize / 2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, (int)(BlockSize / 2));
+            Assert.That(data,Is.All.EqualTo(0),"Data should not have been changed.");
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CountLargerThanRemainingDestinationIsIllegal([Values(1,(int) DefaultBlockSize*2)] int delta)
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize / 2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, count: (int?) (BlockSize/2+delta));
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CountNegativeCountIsIllegal([Values(1, (int) DefaultBlockSize*2)] int delta)
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize / 2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, count: -delta);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ReadNegativeDestinationIsIllegal()
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize / 2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, -1);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ReadNegativeBlockIndexIsIllegal()
+        {
+            CreateSpace();
+
+            var data = new byte[BlockSize / 2];
+            BlockManager.ReadDataBlock((BlockOffset)1, data, blockIndex:-1);
+        }
+
+        [Test]
+        public void TotalBlockCount()
+        {
+            CreateSpace();
+
+            Assert.That(BlockManager.TotalBlockCount,Is.EqualTo(BlockCount));
+        }
+
+        [Test]
+        public void FullBlockReadWrite()
+        {
+            const string sentinel = "There is really n\0thing magical about this string.";
+            var data = Encoding.UTF32.GetBytes(sentinel);
+
+            CreateSpace(blockSize:(uint) data.Length);
+
+            BlockManager.WriteDataBlock((BlockOffset) 2,data);
+
+            var check = new byte[data.Length];
+            BlockManager.ReadDataBlock((BlockOffset) 2,check);
+
+            Assert.That(Encoding.UTF32.GetString(check),Is.EqualTo(sentinel));
+        }
+
+        [Test]
+        public void ReadWriteShort()
+        {
+            const string sentinel = "There is really n\0thing magical about this string.";
+            var data = Encoding.UTF32.GetBytes(sentinel);
+
+            CreateSpace(blockSize: (uint)data.Length + 20u);
+
+            Assert.That(BlockSize > sentinel.Length);
+            BlockManager.WriteDataBlock((BlockOffset)2, data);
+
+            var check = new byte[data.Length];
+            BlockManager.ReadDataBlock((BlockOffset)2, check);
+
+            Assert.That(Encoding.UTF32.GetString(check), Is.EqualTo(sentinel));
+        }
+
+        #region Disposal
+
+        public void Dispose()
+        {
+            if (Space != null)
+            {
+                Space.Dispose();
+                Space = null;
+            }
+
+
+// ReSharper disable SuspiciousTypeConversion.Global
+            var disposable = BlockManager as IDisposable;
+// ReSharper restore SuspiciousTypeConversion.Global
+
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+
+            BlockManager = null;
+        }
+
+        #endregion
+
+    }
+}
