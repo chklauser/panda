f84fff0a20bc4a0fa5db358663389a617264e874
Roger Walt <roger.walt@gmail.com>
2013-04-05 10:57:50 +0200

    added offset capacities to IFileContinuationBlock

--- Panda.Core/Core/Blocks/IFileContinuationBlock.cs
+++ Panda.Core/Core/Blocks/IFileContinuationBlock.cs
@@ -3,5 +3,9 @@ namespace Panda.Core.Blocks
     public interface IFileContinuationBlock : IOffsetListBlock
     {
         void ReplaceOffsets(BlockOffset[] offsets);
+
+        int TotalOffsetCapacity { get; }
+
+        int RemainingOffsetCapacity { get; }
     }
 }
