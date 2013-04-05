4213eae1291a1e44508a7f0bcd33a67c21b3c289
Roger Walt <roger.walt@gmail.com>
2013-04-05 11:07:27 +0200

    offset capacities are not needed in IFileContinuationBlock
    IFileBlock.Count gets number of current block offsets in the FileBlock
    and IOffsetListBlock.ListCapacity gets number of total block offsets.

--- Panda.Core/Core/Blocks/IFileContinuationBlock.cs
+++ Panda.Core/Core/Blocks/IFileContinuationBlock.cs
@@ -3,9 +3,5 @@ namespace Panda.Core.Blocks
     public interface IFileContinuationBlock : IOffsetListBlock
     {
         void ReplaceOffsets(BlockOffset[] offsets);
-
-        int TotalOffsetCapacity { get; }
-
-        int RemainingOffsetCapacity { get; }
     }
 }
