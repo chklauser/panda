696f8f7abbc7e22c063fa7bde77ac28161a7cc75
Roger Walt <roger.walt@gmail.com>
2013-04-08 11:48:19 +0200

    rename ugly, but almost functional

---
+++ Panda.UI/NodeNameValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Panda.UI
+{
+    public class NodeNameValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (VirtualFileSystem.IsLegalNodeName((string) value))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                var msg = string.Format("Node name must not contain a /");
+                return new ValidationResult(false, msg);
+            }
+        }
+    }
+}
